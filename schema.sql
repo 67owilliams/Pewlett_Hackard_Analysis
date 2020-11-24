34c1c3e2cabf47386f5dd825d56e73e689a63548
67owilliams <67owilliams@gmail.com>
2020-11-23 23:12:23 -0600

    Added queries.sql

--- schema.sql
+++ schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE titles (
 	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -48,8 +48,18 @@ CREATE TABLE dept_emp (
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
+--DROP TABLE departments CASCADE;
+--DROP TABLE dept_emp CASCADE;
+--DROP TABLE dept_manager CASCADE;
+--DROP TABLE employees CASCADE;
+--DROP TABLE salaries CASCADE;
+--DROP TABLE titles CASCADE;
+
 SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM titles;
